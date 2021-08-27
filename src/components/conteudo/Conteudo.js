ea3dea097af98bd2cb50c8a2304b47f8372af718
Leandroyyy <leandro.cavallari123@gmail.com>
2021-08-27 11:47:08 -0300

    realizando content

--- src/components/conteudo/Conteudo.js
+++ src/components/conteudo/Conteudo.js
@@ -1,11 +1,33 @@
 import React from "react"
+import Author from "../../img/author.png"
 
 import "./Conteudo.css"
 
 export default function Conteudo(){
     return(
-        <>
-            <h1>hello world</h1>
+        <>      
+            <main className="content">         
+
+                <div className="title">   
+                    <h1>Quais são os sintomas da variante Delta da Covid-19? Teste seus conhecimentos</h1>
+
+                    <hr/>
+                    <p>
+                    A infecção causada pela variante Delta pode provocar sintomas diferentes da doença 
+                    causada pela linhagem original do novo coronavírus
+                    </p>
+                    
+                    <img id="imageAuthor" src={Author} alt="autor"/> 
+
+                    <hr/>
+
+                    <p>
+                    24/08/2021 às 16:44 | Atualizado às 16:56
+                    </p>
+                </div>
+                
+        
+            </main>
         </>
     )
 }
